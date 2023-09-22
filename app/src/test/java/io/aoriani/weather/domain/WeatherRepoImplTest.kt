427c49216764507be8dd00e97d7db21fb8f767f6
André  Oriani <aoriani@aoriani.dev>
2023-09-21 17:16:04 -0700

    Weather Repo: city search

--- app/src/test/java/io/aoriani/weather/domain/WeatherRepoImplTest.kt
+++ app/src/test/java/io/aoriani/weather/domain/WeatherRepoImplTest.kt
@@ -10,12 +10,13 @@ import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import io.aoriani.weather.domain.models.Result
+import io.mockk.coVerify
 import org.junit.Assert.assertSame
 import org.junit.Assert.assertTrue
 
 class WeatherRepoImplTest {
     @Test
-    fun `ui`() = runTest {
+    fun `When city query is not found, search city returns not found`() = runTest {
         val mockNotFoundException: ClientRequestException = mockk {
             every { response } returns mockk {
                 every { status } returns HttpStatusCode.NotFound
@@ -30,7 +31,7 @@ class WeatherRepoImplTest {
     }
 
     @Test
-    fun `uii`() = runTest {
+    fun `When city query faces other issues, search city returns unknown`() = runTest {
         val mockOpenWeatherRestApi: OpenWeatherRestApi = mockk()
         coEvery { mockOpenWeatherRestApi.search(any()) } throws Throwable()
 
@@ -39,15 +40,32 @@ class WeatherRepoImplTest {
         assertSame(Result.Unknown, result)
     }
 
-//    @Test
-//    fun `uiui`() = runTest {
-//        val mockOpenWeatherRestApi: OpenWeatherRestApi = mockk()
-//        coEvery { mockOpenWeatherRestApi.search(any()) } returns mockk<Forecast> {
-//            every {  }
-//        }
-//
-//        val repo = WeatherRepoImpl(mockOpenWeatherRestApi)
-//        val result = repo.searchCity("London")
-//        assertTrue(result is Result.Success)
-//    }
+    @Test
+    fun `When city query success it returns weather, subsequent calls returns cached value`() = runTest {
+        val mockOpenWeatherRestApi: OpenWeatherRestApi = mockk()
+        coEvery { mockOpenWeatherRestApi.search(any()) } returns mockk<Forecast> {
+            every { id } returns 989
+            every { name } returns "Piracicaba"
+            every { main } returns mockk {
+                every { temp } returns 294.817
+                every { tempMin } returns 291.483
+                every { tempMax } returns 299.817
+                every { feelsLike } returns 297.039
+                every { humidity } returns 70
+            }
+            every { weather } returns listOf(mockk {
+                every { id } returns 800
+            })
+        }
+
+        val repo = WeatherRepoImpl(mockOpenWeatherRestApi)
+        val result = repo.searchCity("Piracicaba")
+        assertTrue(result is Result.Success)
+
+        val result2 = repo.searchCity("piracicaba")
+        assertTrue(result2 is Result.Success)
+
+        // API is called once because result is cached
+        coVerify(exactly = 1) { mockOpenWeatherRestApi.search(any())}
+    }
 }
