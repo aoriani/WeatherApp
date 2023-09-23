9b4140402a44aa11608ed0d1caa60625de81623b
André  Oriani <aoriani@aoriani.dev>
2023-09-22 19:14:34 -0700

    Detail Content

--- app/src/main/java/io/aoriani/weather/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/io/aoriani/weather/ui/screens/detail/DetailScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
@@ -23,14 +24,16 @@ import io.aoriani.weather.ui.theme.WeatherAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen() {
+fun DetailScreen(
+    navigateUp: () -> Unit = {}
+) {
     val scrollBehavior =
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            LargeTopAppBar(
+            TopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary,
@@ -39,10 +42,10 @@ fun DetailScreen() {
                     Text(text = "Piracicaba", maxLines = 1, overflow = TextOverflow.Ellipsis)
                 },
                 navigationIcon = {
-                    IconButton(onClick = { /* do something */ }) {
+                    IconButton(onClick = navigateUp) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Localized description"
+                            contentDescription = null
                         )
                     }
                 },
