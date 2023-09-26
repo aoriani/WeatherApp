d96406a4117fd09e93902c8a7afbc18702164562
André Oriani <aoriani@walmartlabs.com>
2023-09-25 20:42:58 -0500

    workshop version

--- app/src/main/java/io/aoriani/weather/ui/navigation/Navigation.kt
+++ app/src/main/java/io/aoriani/weather/ui/navigation/Navigation.kt
@@ -9,34 +9,22 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import io.aoriani.weather.domain.WeatherRepo
 import io.aoriani.weather.ui.screens.detail.DetailScreen
-import io.aoriani.weather.ui.screens.detail.DetailViewModel
-import io.aoriani.weather.ui.screens.detail.DetailViewModelImpl
 import io.aoriani.weather.ui.screens.master.MasterScreen
-import io.aoriani.weather.ui.screens.master.MasterViewModel
-import io.aoriani.weather.ui.screens.master.MasterViewModelImpl
 
 @Composable
 internal fun Navigation(repo: WeatherRepo) {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "master") {
         composable("master") {
-            val viewModel: MasterViewModel =
-                viewModel(factory = MasterViewModelImpl.Factory(repo))
-            MasterScreen(viewModel = viewModel, showWeatherDetails = { cityId ->
-                navController.navigate("detail/$cityId")
-            })
+            MasterScreen()
         }
 
         composable(
-            "detail/{id}",
+            route = "detail/{id}",
             arguments = listOf(navArgument("id") { type = NavType.LongType })
         ) { navBackStackEntry ->
             val id = navBackStackEntry.arguments?.getLong("id") ?: 0L
-            val viewModel: DetailViewModel =
-                viewModel(factory = DetailViewModelImpl.Factory(id, repo))
-            DetailScreen(viewModel = viewModel, navigateUp = {
-                navController.popBackStack()
-            })
+            DetailScreen()
         }
     }
 }
---
+++ app/src/main/java/io/aoriani/weather/ui/screens/master/MasterScreen.kt
@@ -0,0 +1,8 @@
+package io.aoriani.weather.ui.screens.master
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MasterScreen() {}
+
+
